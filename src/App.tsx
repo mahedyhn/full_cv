cc82713ee1bb981c3dbfb668dab98ad97f83bb98
mahedyhn <mehedyhasan78600@gmail.com>
2026-08-18 00:31:21 +0600

    cv

--- src/App.tsx
+++ src/App.tsx
@@ -460,7 +460,7 @@ const App = () => {
             <div className="aspect-[1/1.4] w-full bg-slate-200 relative">
               {/* Replace the src with actual PDF link when uploaded */}
               <iframe
-  src="https://drive.google.com/file/d/1EZtUivvBdJDjme7UtjFDofoxnqxM8NjV/preview"
+  src="https://drive.google.com/file/d/1VNbp2TUehz00myhDRB67pgjUPCKPRTIv/preview"
                 className="w-full h-full border-none"
                 title="Resume Preview"
               ></iframe>
